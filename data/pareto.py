5a82eab6c47ddbe0cfaae79aea5f005bcee97b54
Snootic <snooticonoob@hotmail.com>
2024-12-10 13:45:48 -0300

    refactor: improve ParetoAnalysis class with modular methods

--- data/pareto.py
+++ data/pareto.py
@@ -3,36 +3,48 @@ import pandas as pd
 class ParetoAnalysis:
     def __init__(self, data):
         self.data = pd.DataFrame(data)
+        self._remove_duplicates()
+
+    def _remove_duplicates(self):
         if len(self.data.columns) > 1:
             self.data.drop_duplicates(inplace=True, subset=self.data.columns[0], keep='first')
 
     def pareto_analysis(self):
-        if len(self.data.columns) == 1:
+        num_columns = len(self.data.columns)
+        if num_columns == 1:
             return self._one_column_pareto()
-        elif len(self.data.columns) == 3 or len(self.data.columns) == 6:
+        elif num_columns == 3 or num_columns == 6:
             return self._pareto_with_costs()
         else:
             return self._pareto_from_dataframe()
 
     def _pareto_from_dataframe(self, column=None):
-        df = self.data.iloc[:, :4] if len(self.data.columns) > 4 else self.data.iloc[:, :2]
+        df = self._select_columns()
+        column = self._get_column(df, column)
+        df = self._calculate_percentages(df, column)
+        df = self._add_totals_line(df)
+        return df
 
+    def _select_columns(self):
+        return self.data.iloc[:, :4] if len(self.data.columns) > 4 else self.data.iloc[:, :2]
+
+    def _get_column(self, df, column):
         if isinstance(column, str):
-            column = df[column]
+            return df[column]
         elif isinstance(column, int):
-            column = df.columns[column]
+            return df.columns[column]
         else:
-            column = df.columns[1]
+            return df.columns[1]
 
+    def _calculate_percentages(self, df, column):
         df['Relative Percentage'] = df[column] / df[column].sum() * 100
         df = df.sort_values(by=column, ascending=False)
-        df['Cumulative Percentage'] = df['Relative Percentage'].cumsum() / df['Relative Percentage'].sum() * 100
+        df['Cumulative Percentage'] = df['Relative Percentage'].cumsum()
+        return df
 
-        totals_line = [df[x].sum() if (df[x].dtype in ['int64', 'float64'])
-                        and x != df.columns[-1] else "TOTAL" for x in df.columns]
-
+    def _add_totals_line(self, df):
+        totals_line = [df[x].sum() if (df[x].dtype in ['int64', 'float64']) and x != df.columns[-1] else "TOTAL" for x in df.columns]
         df.loc[-1] = totals_line
-
         return df
 
     def _one_column_pareto(self):
@@ -40,7 +52,6 @@ class ParetoAnalysis:
         df['Values'] = df.groupby(df.columns[0])[df.columns[0]].transform('count')
         df.drop_duplicates(subset=df.columns[0], keep='first', inplace=True)
         self.data = df
-
         return self._pareto_from_dataframe()
 
     def _pareto_with_costs(self):
@@ -48,7 +59,6 @@ class ParetoAnalysis:
         df['Total Cost'] = df[df.columns[1]] * df[df.columns[2]]
         df[df.columns[3]] = df['Total Cost']
         self.data = df
-
         return self._pareto_from_dataframe(3)
 
 if __name__ == "__main__":
